fb0ea4c2842a2351462091e4bef93bbd3c39abb1
aman <bhateriyaaman3@gmail.com>
2026-04-16 10:38:20 +0530

    folder structure fix

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a140d447f1f37cd25f823f840fc90c02466d2947")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd9d37984c6b99a92c85f573ce0daddc9a589891")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ backend/obj/Release/net10.0/PubTmp/EFSQLScripts/AppDbContext.sql
@@ -0,0 +1,66 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260306183517_initialcreate'
+)
+BEGIN
+    CREATE TABLE [DayNotes] (
+        [Id] int NOT NULL IDENTITY,
+        [Date] date NOT NULL,
+        CONSTRAINT [PK_DayNotes] PRIMARY KEY ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260306183517_initialcreate'
+)
+BEGIN
+    CREATE TABLE [Notes] (
+        [Id] int NOT NULL IDENTITY,
+        [Title] nvarchar(max) NOT NULL,
+        [Text] nvarchar(max) NOT NULL,
+        [DayNotesId] int NOT NULL,
+        CONSTRAINT [PK_Notes] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Notes_DayNotes_DayNotesId] FOREIGN KEY ([DayNotesId]) REFERENCES [DayNotes] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260306183517_initialcreate'
+)
+BEGIN
+    CREATE UNIQUE INDEX [IX_DayNotes_Date] ON [DayNotes] ([Date]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260306183517_initialcreate'
+)
+BEGIN
+    CREATE INDEX [IX_Notes_DayNotesId] ON [Notes] ([DayNotesId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260306183517_initialcreate'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260306183517_initialcreate', N'10.0.3');
+END;
+
+COMMIT;
+GO
+
